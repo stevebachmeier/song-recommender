c7832492f694518a21bd5712de8b8c913c03d7ec
Steve Bachmeier <steve.d.bachmeier@gmail.com>
2019-01-22 11:19:40 -0500

    Added exception handling; finished basic script file

---
+++ song_recommender.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+@author: steve
+"""
+
+#==============================================================================
+#
+# IMPORT LIBRARIES
+#
+#==============================================================================
+import dill
+import pandas as pd
+
+#==============================================================================
+#
+# LOAD DATAFRAMES
+#
+#==============================================================================
+df_playlists_train = dill.load(open("df_playlists_train.pkl", "rb"))
+playlist_song_mat_train = dill.load(open("playlist_song_mat_train.pkl", "rb"))     
+
+#==============================================================================
+#
+# RUN INQUIRIES
+#
+#==============================================================================
+# Ask for song or band
+song_band_inquiry = input('Name a song or band: ').lower()
+
+# Show options
+options = pd.DataFrame([col for col in playlist_song_mat_train.columns if song_band_inquiry in col])[0]
+print('Suggestions:')
+print('\n')
+print(options)
+
+# Obtain user input
+while True:
+    print('\n')
+    options
+    try:
+        song_band_number = int(input('What number? '))
+    except:
+        print('\n')
+        print('*** Must input an integer. ***')
+        continue
+    else: 
+        if (song_band_number in range(0,len(options))):
+            song_band_choice = options[song_band_number]
+            break
+        else:
+            print('\n')
+            print('*** Choose a number from the table. ***')
+            continue
+			
+#==============================================================================
+#
+# RECOMMEND
+#
+#==============================================================================
+print('Top 3 suggested songs: ')
+for x_song_band, x_count in \
+    playlist_song_mat_train[playlist_song_mat_train[song_band_choice]==1]\
+        .sum().sort_values(ascending=False)[1:4,].iteritems():
+    print(' * ', x_song_band)
